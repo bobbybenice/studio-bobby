45e6e291a8e6cc7627500e6c3fbcc92a7b256d5e
Robert Pettersson <robert.pettersson@rebelandbird.com>
2023-01-12 20:13:25 +0100

    add celtics image to cases

--- src/constants/cases.js
+++ src/constants/cases.js
@@ -1,75 +1,74 @@
-import DrunkOnAHorse from "../assets/images/drunk-horse.png";
-import HighWithGiraffes from "../assets/images/high-with-giraffes.png";
-import Siesta from "../assets/images/siesta.png";
-import Thirsty from "../assets/images/thirsty.png";
+import DrunkOnAHorse from '../assets/images/drunk-horse.png';
+import HighWithGiraffes from '../assets/images/high-with-giraffes.png';
+import Siesta from '../assets/images/siesta.png';
+import Thirsty from '../assets/images/thirsty.png';
+import Celtics from '../assets/images/celtics.JPG';
 
 const CASES = [
   {
     id: 0,
-    title: "Drunk On A Horse",
-    imageUrl: DrunkOnAHorse,
-    imageAlt: "Drunk on a horse",
+    title: 'Drunk On A Horse',
+    imageUrl: Celtics,
+    imageAlt: 'Drunk on a horse',
   },
   {
     id: 1,
-    title: "High With Giraffes",
+    title: 'High With Giraffes',
     imageUrl: HighWithGiraffes,
-    imageAlt: "High with giraffes",
+    imageAlt: 'High with giraffes',
   },
   {
     id: 2,
-    title: "Siesta",
+    title: 'Siesta',
     imageUrl: Siesta,
-    imageAlt: "Siesta",
+    imageAlt: 'Siesta',
   },
   {
     id: 3,
-    title: "Thirsty",
+    title: 'Thirsty',
     imageUrl: Thirsty,
-    imageAlt: "Thirsty",
+    imageAlt: 'Thirsty',
   },
   {
     id: 4,
-    title: "Drunk On A Horse",
+    title: 'Drunk On A Horse',
     imageUrl: DrunkOnAHorse,
-    imageAlt: "Drunk on a horse",
+    imageAlt: 'Drunk on a horse',
   },
   {
     id: 5,
-    title: "Studio Bobby",
+    title: 'Studio Bobby',
   },
   {
     id: 6,
-    title: "High With Giraffes",
+    title: 'High With Giraffes',
     imageUrl: HighWithGiraffes,
-    imageAlt: "High with giraffes",
+    imageAlt: 'High with giraffes',
   },
   {
     id: 7,
-    title: "Siesta",
+    title: 'Siesta',
     imageUrl: Siesta,
-    imageAlt: "Siesta",
+    imageAlt: 'Siesta',
   },
   {
     id: 8,
-    title: "Thirsty",
+    title: 'Thirsty',
     imageUrl: Thirsty,
-    imageAlt: "Thirsty",
+    imageAlt: 'Thirsty',
   },
   {
     id: 9,
-    title: "Siesta",
+    title: 'Siesta',
     imageUrl: Siesta,
-    imageAlt: "Siesta",
+    imageAlt: 'Siesta',
   },
   {
     id: 10,
-    title: "Thirsty",
+    title: 'Thirsty',
     imageUrl: Thirsty,
-    imageAlt: "Thirsty",
-  }
+    imageAlt: 'Thirsty',
+  },
 ];
 
-export {
-  CASES,
-}
+export { CASES };
